145993b6f679f1dc81b49aa60eb87959a9315051
vikranth <vikranthyadlapalli123@gmail.com>
2025-11-01 16:38:29 +0530

    Abstraction with example

--- Abstraction.java
+++ Abstraction.java
@@ -15,3 +15,18 @@ class Dog extends Animal{
         System.out.println("Bow Bow");
     }
 }
+
+public class Abstraction {
+   public Abstraction() {
+   }
+
+   public static void main(String[] var0) {
+      Dog var1 = new Dog();
+    //calling abstract method
+    //implementation of abstract method  
+     //runtime polymorphism
+     // dynamic method dispatch
+      var1.sound();   //output: Bow Bow
+      var1.sleep();    //output: Sleeping...
+   }
+}
